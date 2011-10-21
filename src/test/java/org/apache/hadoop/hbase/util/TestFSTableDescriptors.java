f528bac84f293fa9115f11bdd878a242c5066c2d
Todd Lipcon <todd@cloudera.com>
2011-10-20 19:49:17 -0700

    Revert "Make TestFSTableDescriptors pass if cache hits are >= rather than ==; I had test the wrong way around"
    This reverts commit 619596f167a2c57ce5b7c8a251787c1373766d03.

--- src/test/java/org/apache/hadoop/hbase/util/TestFSTableDescriptors.java
+++ src/test/java/org/apache/hadoop/hbase/util/TestFSTableDescriptors.java
@@ -112,12 +112,10 @@ public class TestFSTableDescriptors {
       assertTrue(htds.get(Bytes.toBytes(name + i)) !=  null);
     }
     assertEquals(count * 4, htds.invocations);
-    assertTrue("expected=" + (count * 2) + ", actual=" + htds.cachehits,
-      htds.cachehits >= (count * 2));
+    assertTrue(count * 2 >= htds.cachehits);
     assertTrue(htds.get(HConstants.ROOT_TABLE_NAME) != null);
     assertEquals(htds.invocations, count * 4 + 1);
-    assertTrue("expected=" + ((count * 2) + 1) + ", actual=" + htds.cachehits,
-      htds.cachehits >= ((count * 2) + 1));
+    assertTrue(htds.cachehits >= count * 2 + 1);
   }
 
   @Test (expected=java.io.FileNotFoundException.class)
